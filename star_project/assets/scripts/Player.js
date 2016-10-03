8e82569d91e2051f0456eb4da032cc24ebcb0a64
glyredsun <glyredsun@163.com>
2016-10-03 21:23:15 +0800

    add player

--- star_project/assets/scripts/Player.js
+++ star_project/assets/scripts/Player.js
@@ -12,15 +12,25 @@ cc.Class({
         //    readonly: false,    // optional, default is false
         // },
         // ...
+        jumpDuration : 0,
+        jumpHeight : 0,
+        axMoveSpeed : 0,
+        accel : 0
     },
 
     // use this for initialization
     onLoad: function () {
-
+        this.node.runAction(this.getJumpAction());
     },
 
     // called every frame, uncomment this function to activate update callback
     // update: function (dt) {
 
     // },
+    
+    getJumpAction : function () {
+        var jumpUp = cc.MoveBy(this.jumpDuration, cc.p(0, this.jumpHeight)).easing(cc.easeCubicActionOut());
+        var jumpDown = cc.MoveBy(this.jumpDuration, cc.p(0, -this.jumpHeight)).easing(cc.easeCubicActionIn());
+        return cc.repeatForever(cc.sequence(jumpUp, jumpDown));
+    }
 });
